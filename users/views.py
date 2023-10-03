07e2f581f242464ce473768f0c20c3538c966c63
RadwanHegazy <radwangaber22@gmail.com>
2023-10-03 23:28:54 +0300

    code

--- users/views.py
+++ users/views.py
@@ -1,9 +1,55 @@
-from django.shortcuts import render, HttpResponse
-
-
+from django.shortcuts import render, redirect
+from users.models import User
+from django.contrib.auth.views import auth_login
 
 def login (request) : 
-    return HttpResponse('login')
+    context = {}
+
+    if request.method == "POST" : 
+
+        try : 
+            data = {
+                'email' : request.POST.get('email'),
+                'password' : request.POST.get('password'),
+            }
+            
+            u = User.login(**data)
+
+            print(u)
+            if u['errors'] == '' :
+                u = u['user']
+                auth_login(request,user=u)
+
+                return redirect('profile')
+            else:
+                context['error'] = u['errors']
+
+        except Exception as e : 
+            context['error'] = f'Error : {e}'
+
+    return render(request,'auth/login.html', context) 
+
 
 def register (request) : 
-    return HttpResponse('register')
+    
+    context = {}
+
+    if request.method == "POST" : 
+
+        try : 
+            data = {
+                'full_name' : request.POST.get('full_name'),
+                'email' : request.POST.get('email'),
+                'password' : request.POST.get('password'),
+            }
+            
+            u = User.objects.create_user(**data)
+
+            auth_login(request,user=u)
+
+            return redirect('profile')
+        except Exception as e : 
+            context['error'] = f'Error : {e}'
+        
+    return render(request,'auth/register.html',context) 
+
